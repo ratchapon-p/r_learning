a89da296266b4f819de47abe9d87c6889784fcb9
unknown <ratchapon.p01@gmail.com>
2024-01-07 11:38:58 +0700

    File

---
+++ Logic.R
@@ -0,0 +1,28 @@
+#logical:
+#TRUE T
+#FALSE F
+
+4 < 5
+10 > 100
+4 == 5
+
+# ==
+# !=
+# >
+# <
+# <=
+# >=
+# !
+# |
+# &
+# isTrue(x)
+
+result <- 4 < 5
+result
+
+result2 <- !T
+
+result | result2
+result & result2
+
+isTRUE(result2)
---
+++ Reminder.R
@@ -0,0 +1,18 @@
+#reminder
+
+rnorm()
+c()
+seq(10, 20, 5)
+rep()
+print()
+
+is.numeric()
+is.integer()
+is.double()
+is.character()
+
+tpyeof()
+
+sqrt()
+paste()
+
